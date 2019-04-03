674b0939f2950baa794b270b92ae2e176523fac6
FelixAbeln <49161958+FelixAbeln@users.noreply.github.com>
2019-04-03 12:02:31 +0200

    Empty C-code project

---
+++ Construction/Construction/main.c
@@ -0,0 +1,18 @@
+/*
+ * Construction.c
+ *
+ * Created: 03/04/2019 12:00:01
+ * Author : Group 1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
